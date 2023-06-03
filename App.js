9f649ae9ca281e667467f892eb220b37b45a4afd
anibalmarquesto <135396894+anibalmarquesto@users.noreply.github.com>
2023-06-02 22:37:25 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Product from "./components/Product";
+import Nabvar from "./components/Nabvar";
+import Products from "./components/Products";
+
+function App() {
+  return (
+    <>
+      <Nabvar />
+      <Product/>
+      <Products />
+      
+    </>
+  );
+}
+
+export default App;
